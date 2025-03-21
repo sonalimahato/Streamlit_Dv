008ef06d8aa9f9fc20a32eef68ad5116ef51b72b
sonali <150420309+sonalimahato@users.noreply.github.com>
2025-03-21 12:08:41 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import main as st 
-import pandas as pd 
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+st.set_page_config(page_title="Data Visualization With Streamlit",
+                   page_icon="😁")
+
+st.write("HI this is new changes")
+st.title("Data Visualization With Streamlit")
+with st.sidebar:
+  st.title("Data Visualization With Streamlit")
+  upload=st.file_uploader("Upload CSV")
+
+if upload is not None:
+    df=pd.read_csv(upload)
+    st.dataframe(df.head())
